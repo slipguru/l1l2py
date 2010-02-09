80d5a57cfccdc050f40e131566282b55650e02c0
Salvatore Masecchia <salvatore.masecchia@disi.unige.it>
2010-02-09 11:52:39 +0100

    features selection function, first draft

--- framework.py
+++ framework.py
@@ -1,34 +1,38 @@
 from __future__ import division
-import tools
 import numpy as np
 
-from algorithms import *
+import algorithms as alg
 import tools
 
-
-def stage_I(X, Y, mu_fact, tau_range, lambda_range=np.empty(0),
-            k=0, experiment_type='classification',
-            standardize_X=True, center_Y=True):
+# l1l2_kcv!
+def select_features(X, Y, mu_fact, tau_range, lambda_range=np.empty(0),
+                    int_cv_sets=None, experiment_type='classification',
+                    standardize_X=True, center_Y=True):
     """
     Ia: mu_fact, tau_range
     Ib: lambda_range on selected
         ** step II -> out **
         
     Normalization inside!
-    Is needed to normalize the single sub-subset
+    Is needed to normalize single sub-subsets
     """
-    int_cv_sets = tools.kcv_indexes(Y, k, experiment_type)
+    if experiment_type =='classification':
+    #    int_cv_sets = tools.classification_splits(Y, k)
+        compute_error = tools.classification_error
+    else:
+    #    int_cv_sets = tools.regression_splits(Y, k)
+        compute_error = tools.regression_error
     
     # --------------------------------------------------
-    print 'Numero fold interni:', len(int_cv_sets)
-    print 'Coppie sets (interno):', int_cv_sets
+    #print 'Numero fold interni:', len(int_cv_sets)
+    #print 'Coppie sets (interno):', int_cv_sets
     # --------------------------------------------------
     
     err_ts = np.empty((len(int_cv_sets), tau_range.size, lambda_range.size))
     err_tr = np.empty_like(err_ts)
     
     for i, (train_idxs, test_idxs) in enumerate(int_cv_sets):
-        print 'Train: ', train_idxs, 'on: ', test_idxs
+        #print 'Train: ', np.asarray(train_idxs)+1, 'on: ', np.asarray(test_idxs)+1
         
         # This command makes copy of the data!
         if standardize_X:
@@ -48,19 +52,18 @@ def stage_I(X, Y, mu_fact, tau_range, lambda_range=np.empty(0),
         for j, b in enumerate(beta_casc):
             selected = (b.flat != 0)
             for k, l in enumerate(lambda_range):
-                beta = ridge_regression(Xtr[:,selected], Ytr, l)
+                beta = alg.ridge_regression(Xtr[:,selected], Ytr, l)
                 
                 labelsTs = Yts + meanY
                 predictedTs = np.dot(Xts[:,selected], beta) + meanY
-                err_ts[i, j, k] = prediction_error(labelsTs,
-                                                   predictedTs,
-                                                   experiment_type)
+                err_ts[i, j, k] = compute_error(labelsTs,
+                                                   predictedTs)
                 
                 labelsTr = Ytr + meanY
                 predictedTr = np.dot(Xtr[:,selected], beta) + meanY
-                err_tr[i, j, k] = prediction_error(labelsTr,
-                                                   predictedTr,
-                                                   experiment_type)
+                err_tr[i, j, k] = compute_error(labelsTr,
+                                                predictedTr)
+    #print err_ts
     err_ts = err_ts.mean(axis=0)
     err_tr = err_tr.mean(axis=0)
        
@@ -70,31 +73,31 @@ def stage_I(X, Y, mu_fact, tau_range, lambda_range=np.empty(0),
           
     return tau_opt, lambda_opt
 
-def stage_II(Xtr, Ytr, Xts, Yts, tau_opt, lambda_opt, mu_range, experiment_type,
-             standardize_X=True, center_Y=True):
-    
-    # This command makes copy of the data!
-    if standardize_X:
-        Xtr, Xts = tools.standardize(Xtr, Xts)
-    if center_Y:
-        Ytr, Yts, meanY = tools.center(Ytr, Yts)
-    
-    # IIa
-    beta_0 = ridge_regression(Xtr, Ytr)
-    beta, k = elastic_net(Xtr, Ytr, mu_range[0], tau_opt, beta_0)
-    selected = (beta.flat != 0)
-    
-    # IIb
-    beta_opt = ridge_regression(Xtr[:,selected], Ytr, lambda_opt)
-    
-    labels = Yts + meanY
-    predicted = np.dot(Xts[:,selected], beta_opt) + meanY
-    err_test = prediction_error(labels, predicted, experiment_type)
-    #sums sums sums
-    
-    for m in mu_range[1:]:
-        pass
-    
-    mu_opt = None
-    return mu_opt
+#def build_classifier(Xtr, Ytr, Xts, Yts, tau_opt, lambda_opt, mu_range, experiment_type,
+#             standardize_X=True, center_Y=True):
+#    
+#    # This command makes copy of the data!
+#    if standardize_X:
+#        Xtr, Xts = tools.standardize(Xtr, Xts)
+#    if center_Y:
+#        Ytr, Yts, meanY = tools.center(Ytr, Yts)
+#    
+#    # IIa
+#    beta_0 = ridge_regression(Xtr, Ytr)
+#    beta, k = elastic_net(Xtr, Ytr, mu_range[0], tau_opt, beta_0)
+#    selected = (beta.flat != 0)
+#    
+#    # IIb
+#    beta_opt = ridge_regression(Xtr[:,selected], Ytr, lambda_opt)
+#    
+#    labels = Yts + meanY
+#    predicted = np.dot(Xts[:,selected], beta_opt) + meanY
+#    err_test = prediction_error(labels, predicted, experiment_type)
+#    #sums sums sums
+#    
+#    for m in mu_range[1:]:
+#        pass
+#    
+#    mu_opt = None
+#    return mu_opt
 
---
+++ tests/test_framework.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+import scipy.io as sio
+from mlabwrap import mlab
+
+from nose.tools import *
+import framework as fw
+import tools
+
+mlab.addpath('tests/matlab_code')
+TOL = 1e-3
+
+class TestFramework(object):
+    """
+    Results generated with the original matlab code
+    """
+    
+    def setup(self):
+        data = sio.loadmat('tests/toy_dataA.mat', struct_as_record=False)
+        self.X = data['X']
+        self.Y = data['Y']
+        self.tau_range = tools.linear_range(0.1, 1.0, 5)
+        self.lambda_range = tools.linear_range(0.1, 1.0, 5)
+        self.mu = 0.1
+        
+    def test_data(self):
+        assert_equals((30, 40), self.X.shape)
+        assert_equals((30, 1), self.Y.shape)
+       
+    def test_select_features(self):
+        for mu in tools.linear_range(0.1, 1.0, 10):
+            tau_opt_exp, lambda_opt_exp = \
+                                mlab.l1l2_kcv(self.X, self.Y, self.tau_range,
+                                              self.lambda_range, mu,
+                                             #K, type, split, mean, col
+                                              5, 'regr', 0,    1,    1, nout=2)
+                           
+            sets = TestFramework._get_matlab_splitting(self.Y, 5)    
+            tau_opt, lambda_opt = \
+                                fw.select_features(self.X, self.Y, mu,
+                                                   self.tau_range, self.lambda_range,
+                                                   int_cv_sets=sets,
+                                                   experiment_type='regression',
+                                                   standardize_X=True,
+                                                   center_Y=True)
+                                                   
+            assert_almost_equals(tau_opt_exp, tau_opt)
+            assert_almost_equals(lambda_opt_exp, lambda_opt)
+          
+    @staticmethod
+    def _get_matlab_splitting(labels, K):
+        sets2 = mlab.splitting(labels, 5, 0)
+        sets2 = mlab.double(mlab.cell2mat(sets2)); #each column = Test Set
+        sets2 -=1 #matlab starts from 1
+        sets = list()
+        indexes = np.arange(labels.size)
+        for ts in sets2.T: #loop on columns
+            ts = np.array(ts, dtype=np.int)
+            tr = np.array(list(set(indexes) - set(ts)))
+            sets.append((tr, ts))
+            
+        return sets
+          
+       
+    #def test_geometric_ranges(self):
+    #    geom_params = np.array([0.1, 1.0, 10]).reshape((3, 1))       
+    #    exp_geom = mlab.range_values(geom_params)
+    #    geom = tools.geometric_range(0.1, 1.0, 10)
+    #    assert_true(np.allclose(exp_geom, geom))
+    
+    

Fixed the normalization functions outputs

data_tools.py
@@ -10,26 +10,32 @@ def geometric_range(min, max, number):
 def standardize(matrix, p=None, return_factors=False):
     mean = matrix.mean(axis=0)
     std = matrix.std(axis=0, ddof=1)
-        
-    if p is None:
-        out = ((matrix - mean)/std,)
-    else:
-        out = ((matrix - mean)/std, (p - mean)/std)
-        
-    if return_factors:
-        return out + (mean, std)
-    else:
-        return out
 
+    # Simple case
+    if p is None and return_factors is False:
+        return (matrix - mean)/std
+                
+    if p is None: # than return_factors is True
+        return (matrix - mean)/std, mean, std
+        
+    if return_factors is False: # ... with p not None
+        return (matrix - mean)/std, (p - mean)/std
+        
+    # Full case
+    return (matrix - mean)/std, (p - mean)/std, mean, std
+    
 def center(matrix, p=None, return_mean=False):
     mean = matrix.mean(axis=0)
     
-    if p is None:
-        out = (matrix - mean,)
-    else:
-        out = (matrix - mean, p - mean)
-        
-    if return_mean:
-        return out + (mean,)
-    else:
-        return out
+    # Simple case
+    if p is None and return_mean is False:
+        return matrix - mean
+    
+    if p is None: # than return_mean is True
+        return (matrix - mean, mean)
+    
+    if return_mean is False: # ...with p not None
+        return (matrix - mean, p - mean)
+    
+    # Full case
+    return (matrix - mean, p - mean, mean)

tests/test_data_tools.py
@@ -50,7 +50,7 @@ class TestDataTools(object):
         assert_true(np.allclose(Ycent, Ycent2))
         
     def test_centering_outputs(self):
-        assert_equals(1, len(center(self.Y)))
+        assert_equals(np.ndarray, type(center(self.Y)))
         assert_equals(2, len(center(self.Y, self.Y)))
         assert_equals(2, len(center(self.Y, return_mean=True)))
         assert_equals(3, len(center(self.Y, self.Y, return_mean=True)))
@@ -67,7 +67,7 @@ class TestDataTools(object):
         assert_true(np.allclose(Xexp, Xexp2))
         
     def test_standardization_outputs(self):
-        assert_equals(1, len(standardize(self.X)))
+        assert_equals(np.ndarray, type(standardize(self.X)))
         assert_equals(2, len(standardize(self.X, self.X)))
         assert_equals(3, len(standardize(self.X, return_factors=True)))
         assert_equals(4, len(standardize(self.X, self.X, return_factors=True)))

tests/test_error_functions.py
@@ -59,7 +59,7 @@ class TestErrorFunctions(object):
         for imbalance in np.linspace(0, 30, 4):
             labels[0:imbalance,:] = -1
 
-            labels_centered = center(labels)[0]            
+            labels_centered = center(labels)
             TestErrorFunctions._test_balanced(self.X, labels)
             TestErrorFunctions._test_balanced(self.X, labels_centered)
     
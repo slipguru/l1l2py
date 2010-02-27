e0e54f7d67b25d827ed0c2a651422a29b1a677ec
Salvatore Masecchia <salvatore.masecchia@gmail.com>
2010-02-27 14:26:31 +0100

    whitespaces removed

--- _core.py
+++ _core.py
@@ -4,16 +4,16 @@ from tools import *
 
 __all__ = ['model_selection',
            'minimal_model', 'nested_lists']
-   
+
 def model_selection(data, labels, test_data, test_labels,
                     mu_range, tau_range, lambda_range,
-                    cv_splits, error_function, 
+                    cv_splits, error_function,
                     data_normalizer=None, labels_normalizer=None,
                     returns_kcv_errors=False):
     """Short Summary.
-    
+
     Extended Summary.
-    
+
     Parameters
     ----------
     name1 :  array_like
@@ -22,20 +22,20 @@ def model_selection(data, labels, test_data, test_labels,
         Description of `name2`
     name3 : {1, 2, 3}
         Description of `name3`
-    
+
     Returns
     -------
     name :  type
         description
-        
+
     Notes
     -----
     Eventually notes and LaTeX [1]_ formulae
-    
+
     .. math::
-    
+
         \sum_{i=1}^N = 1
-        
+
     References
     ----------
     .. [1] "Guide to LaTEX", foo editions
@@ -44,27 +44,27 @@ def model_selection(data, labels, test_data, test_labels,
     --------
     >>> np.add(1, 2)
     3
-    
+
     Second example
-    
+
     >>> np.add([1, 2], [3, 4])
     array([4, 6])
-    
+
     """
-    
+
     stage1_out = minimal_model(data, labels, mu_range[0],
                                tau_range, lambda_range,
                                cv_splits, error_function,
                                data_normalizer, labels_normalizer,
                                returns_kcv_errors)
     tau_opt, lambda_opt = stage1_out[0:2]
-    
+
     stage2_out = nested_lists(data, labels,
                               test_data, test_labels,
                               tau_opt, lambda_opt, mu_range,
                               error_function,
                               data_normalizer, labels_normalizer)
-    
+
     return stage1_out + stage2_out
 
 def minimal_model(data, labels, mu, tau_range, lambda_range,
@@ -72,45 +72,45 @@ def minimal_model(data, labels, mu, tau_range, lambda_range,
                   data_normalizer=None, labels_normalizer=None,
                   returns_kcv_errors=False):
     """ TODO: add docstring """
-    
+
     err_ts = list()
     err_tr = list()
     max_tau_num = len(tau_range)
-    
-    for train_idxs, test_idxs in cv_splits:        
+
+    for train_idxs, test_idxs in cv_splits:
         # First create a view and then normalize (eventually)
         data_tr, data_ts = data[train_idxs,:], data[test_idxs,:]
         if not data_normalizer is None:
             data_tr, data_ts = data_normalizer(data_tr, data_ts)
-            
+
         labels_tr, labels_ts = labels[train_idxs,:], labels[test_idxs,:]
         if not labels_normalizer is None:
             labels_tr, labels_ts = labels_normalizer(labels_tr, labels_ts)
-            
+
         # Builds a classifier for each value of tau
         beta_casc = elastic_net_regpath(data_tr, labels_tr, mu,
                                         tau_range[:max_tau_num])
-        
+
         if len(beta_casc) < max_tau_num: max_tau_num = len(beta_casc)
         _err_ts = np.empty((len(beta_casc), len(lambda_range)))
         _err_tr = np.empty_like(_err_ts)
-        
+
         # For each sparse model builds a
         # rls classifier for each value of lambda
         for j, b in enumerate(beta_casc):
             selected = (b.flat != 0)
             for k, lam in enumerate(lambda_range):
                 beta = ridge_regression(data_tr[:,selected], labels_tr, lam)
-                
+
                 prediction = np.dot(data_ts[:,selected], beta)
                 _err_ts[j, k] = error_function(labels_ts, prediction)
-    
+
                 prediction = np.dot(data_tr[:,selected], beta)
                 _err_tr[j, k] = error_function(labels_tr, prediction)
-        
+
         err_ts.append(_err_ts)
         err_tr.append(_err_tr)
-    
+
     # cut columns and computes the mean
     err_ts = np.asarray([a[:max_tau_num] for a in err_ts]).mean(axis=0)
     err_tr = np.asarray([a[:max_tau_num] for a in err_tr]).mean(axis=0)
@@ -118,7 +118,7 @@ def minimal_model(data, labels, mu, tau_range, lambda_range,
     tau_opt_idx, lambda_opt_idx = np.where(err_ts == err_ts.min())
     tau_opt = tau_range[tau_opt_idx[0]]             # ?? [0] or [-1]
     lambda_opt = lambda_range[lambda_opt_idx[0]]
-    
+
     if returns_kcv_errors:
         return tau_opt, lambda_opt, err_ts, err_tr
     else:
@@ -128,37 +128,36 @@ def nested_lists(data, labels, test_data, test_labels,
                  tau, lambda_, mu_range, error_function=None,
                  data_normalizer=None, labels_normalizer=None):
     """ TODO: add docstring """
-    
+
     if not data_normalizer is None:
         data, test_data = data_normalizer(data, test_data)
-            
+
     if not labels_normalizer is None:
-        labels, test_labels = labels_normalizer(labels, test_labels)  
-    
+        labels, test_labels = labels_normalizer(labels, test_labels)
+
     beta_list = list()
     selected_list = list()
     if error_function:
         err_tr_list = list()
         err_ts_list = list()
-    
-    for mu in mu_range:        
+
+    for mu in mu_range:
         beta = elastic_net(data, labels, mu, tau)
         selected = (beta.flat != 0)
-    
+
         beta = ridge_regression(data[:,selected], labels, lambda_)
-           
-        if error_function:    
+
+        if error_function:
             prediction = np.dot(data[:,selected], beta)
             err_tr_list.append(error_function(labels, prediction))
-            
+
             prediction = np.dot(test_data[:,selected], beta)
             err_ts_list.append(error_function(test_labels, prediction))
 
         beta_list.append(beta)
         selected_list.append(selected)
-                      
+
     if error_function:
         return beta_list, selected_list, err_tr_list, err_ts_list
     else:
         return beta_list, selected_list
-
--- algorithms.py
+++ algorithms.py
@@ -26,34 +26,34 @@ def ridge_regression(data, labels, mu=0.0):
         Labels vector.
     mu : float, optional (default is `0.0`)
         :math:`\ell_2` norm penalty.
-    
+
     Returns
     -------
     beta : (D,) or (D, 1) ndarray
         RLS model.
-    
+
     Notes
     -----
-    RLS minimizes the following objective function:    
-    
+    RLS minimizes the following objective function:
+
     .. math::
-    
+
         \frac{1}{N} \| Y - X\beta \|_2^2 + \mu \|\beta\|_2^2
-    
+
     finding the optimal model :math:`\beta^*`, where
-    
+
     =============== ===============
     :math:`X`       ``data``
     --------------- ---------------
     :math:`Y`       ``labels``
     --------------- ---------------
-    :math:`\mu`     ``mu``      
+    :math:`\mu`     ``mu``
     --------------- ---------------
-    :math:`\beta^*` ``beta``    
+    :math:`\beta^*` ``beta``
     =============== ===============
-    
+
     Using ``mu`` = `0.0` the algorithm performs Ordinary Least Squares (OLS).
-       
+
     Examples
     --------
     >>> X = numpy.array([[0.1, 1.1, 0.3], [0.2, 1.2, 1.6], [0.3, 1.3, -0.6]])
@@ -61,24 +61,24 @@ def ridge_regression(data, labels, mu=0.0):
     >>> y = numpy.dot(X, beta)
     >>> biolearning.algorithms.ridge_regression(X, y)
     array([  1.00000000e-01,   1.00000000e-01,   6.62515290e-17])
-    
+
     """
     n, d = data.shape
-        
+
     if n < d:
         tmp = np.dot(data, data.T)
         if mu:
             tmp += mu*n*np.eye(n)
         tmp = np.linalg.pinv(tmp)
-        
+
         return np.dot(np.dot(data.T, tmp), labels)
     else:
         tmp = np.dot(data.T, data)
         if mu:
             tmp += mu*n*np.eye(d)
         tmp = np.linalg.pinv(tmp)
-        
-        return np.dot(tmp, np.dot(data.T, labels))        
+
+        return np.dot(tmp, np.dot(data.T, labels))
 
 def elastic_net_regpath(data, labels, mu, tau_range, beta=None, kmax=1e5,
                         step_size=None):
@@ -86,7 +86,7 @@ def elastic_net_regpath(data, labels, mu, tau_range, beta=None, kmax=1e5,
 
     Find the EN regularization path for each value in ``tau_range`` and
     fixed value of ``mu``.
-            
+
     Parameters
     ----------
     data : (N, D) ndarray
@@ -106,47 +106,47 @@ def elastic_net_regpath(data, labels, mu, tau_range, beta=None, kmax=1e5,
         Iterations step size.
         If is `None` the algorithm use default value
         (see :func:`elastic_net` `Notes`).
-    
+
     Returns
     -------
     beta_path : list of (D,) or (D, 1) ndarray
         EN models. The number of models can differ the number of `tau` values.
         The functions returns only the model with at least one nonzero value.
         For very high value of tau a model can have all `0s`.
-    
+
     See Also
     --------
     elastic_net
-    
+
     """
     from collections import deque
     n, d = data.shape
-    
+
     if mu == 0.0:
         beta_ls = ridge_regression(data, labels)
     if beta is None:
         beta = np.zeros((d, 1))
-    
+
     out = deque()
     nonzero = 0
     for tau in reversed(tau_range):
-        if mu == 0.0 and nonzero >= n: # lasso saturation             
+        if mu == 0.0 and nonzero >= n: # lasso saturation
             beta_next = beta_ls
         else:
             beta_next = elastic_net(data, labels, mu, tau, beta,
                                     kmax, step_size)
-          
+
         if len(beta_next.nonzero()[0]) > 0:
             out.appendleft(beta_next)
-            
+
         beta = beta_next
-                   
+
     return out
 
 def elastic_net(data, labels, mu, tau, beta=None, kmax=1e5, step_size=None,
                 returns_iterations=False):
     r"""Elastic Net regularization.
-    
+
     Finds the EN model with ``mu`` parameter associated with its
     :math:`\ell_2` norm and ``tau`` parameter associated with its
     :math:`\ell_1` norm (see `Notes`).
@@ -173,59 +173,59 @@ def elastic_net(data, labels, mu, tau, beta=None, kmax=1e5, step_size=None,
         If True, return the number of iterations performed.
         The algorithm has a predefined minimum number of iterations
         equal to `100`.
-        
+
     Returns
     -------
     beta : (D,) or (D, 1) ndarray
         EN model.
     k : int, optional
         Number of iterations performed.
-    
+
     Notes
-    -----    
-    EN minimizes the following objective function:    
-    
+    -----
+    EN minimizes the following objective function:
+
     .. math::
-    
+
         \frac{1}{N} \| Y - X\beta \|_2^2 + \mu \|\beta\|_2^2 + \tau \|\beta\|_1
-    
+
     finding the optimal model :math:`\beta^*`, where
-    
+
     =============== ===============
     :math:`X`       ``data``
     --------------- ---------------
     :math:`Y`       ``labels``
     --------------- ---------------
-    :math:`\mu`     ``mu``      
+    :math:`\mu`     ``mu``
     --------------- ---------------
-    :math:`\beta^*` ``beta``    
+    :math:`\beta^*` ``beta``
     =============== ===============
-    
+
     Using ``mu`` = `0.0` the algorithm performs LASSO.
-    
+
     The computation is iterative, each step updates the value of beta until
     the convergence is reached:
-    
+
     .. math::
-        
+
         \beta^{(k+1)} = \frac{1}{1 + \frac{N\mu}{C}}
                         \mathbf{S}_{\frac{N\tau}{C}} (
                             \beta^k + \frac{1}{C}[X^TY - X^TX\beta^k]
                         )
-    
-    
+
+
     where we have to choice :math:`\frac{1}{C} < \frac{2}{\|X^T X\|}`.
-    
+
     The default value is close to the maximum step size:
-    
+
     .. math::
-    
+
         \frac{1}{C} = \frac{2}{\|X^T X\| + 10^{-5}} < \frac{2}{\|X^T X\|}
-    
+
     See Also
     --------
     elastic_net_regpath
-    
+
     Examples
     --------
     >>> X = numpy.array([[0.1, 1.1, 0.3], [0.2, 1.2, 1.6], [0.3, 1.3, -0.6]])
@@ -235,19 +235,19 @@ def elastic_net(data, labels, mu, tau, beta=None, kmax=1e5, step_size=None,
     array([  1.00000000e-01,   1.00000000e-01,   6.24500451e-17])
     >>> biolearning.algorithms.elastic_net(X, y, 0.1, 0.1)
     array([ 0.        ,  0.07715517,  0.        ])
-    
+
     """
     n = data.shape[0]
-    
+
     if step_size is None:
         step_size = _step_size(data)    # 1/C
     mu_s = (n * mu) * step_size         # (n mu)/C
     tau_s = (n * tau) * step_size       # (n tau)/C
-    
+
     # Initializations
     XT = data.T * step_size             # X^T / C
     XTY = np.dot(XT, labels)            # (X^T Y) / C
-           
+
     if beta is None:
         beta = ridge_regression(data, labels)
 
@@ -255,10 +255,10 @@ def elastic_net(data, labels, mu, tau, beta=None, kmax=1e5, step_size=None,
     th, difference = -np.inf, np.inf
     while k < kmin or ((difference > th).any() and k < kmax):
         k += 1
-        
+
         value = beta +  XTY - np.dot(XT, np.dot(data, beta))
         beta_next = _soft_thresholding(value, tau_s) / (1.0 + mu_s)
-                        
+
         difference = np.abs(beta_next - beta)
         th = np.abs(beta) * (tol / k)
         beta = beta_next
@@ -270,15 +270,15 @@ def elastic_net(data, labels, mu, tau, beta=None, kmax=1e5, step_size=None,
 
 def _step_size(matrix):
     n, d = matrix.shape
-    
+
     if d > n:
         tmp = np.dot(matrix, matrix.T)
     else:
         tmp = np.dot(matrix.T, matrix)
     max_eig = np.linalg.eigvalsh(tmp).max()
-    
+
     return 2.0/(max_eig + 1e-5)
-      
+
 def _soft_thresholding(x, th):
     """ TODO: Add docstring """
     out = x - (np.sign(x) * (th / 2.0))

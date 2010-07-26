c771ab57f1047c94e84ba470782fdeb9abab59cd
Salvatore Masecchia <salvatore.masecchia@disi.unige.it>
2010-07-26 10:04:17 +0200

    improved version generation

--- l1l2py/_version.py
+++ l1l2py/_version.py
@@ -1,4 +1,13 @@
+def _get_version(version, status=None):
+    if status:
+        import datetime
+        now = datetime.datetime.now()
+        date = '.'.join('%02d'%x for x in (now.year, now.month, now.day))
+        return '%s%s-%s' % (version, status, date)
+    return version
+
 # major number for main changes
 # minor number for new features
 # release number for bug fixes and minor updates
-version = '1.0.2alpha'
+# status = {'alpha', 'beta', None}
+version = _get_version('1.0.2', status='alpha')

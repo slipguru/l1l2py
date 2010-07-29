1844d447220be36da18deae556cef1fb883012f7
Salvatore Masecchia <salvatore.masecchia@disi.unige.it>
2010-07-29 11:33:11 +0200

    1.0.2 preparation

--- l1l2py/_version.py
+++ l1l2py/_version.py
@@ -10,4 +10,4 @@ def _get_version(version, status=None):
 # minor number for new features
 # release number for bug fixes and minor updates
 # status = {'alpha', 'beta', None}
-version = _get_version('1.0.2', status='alpha')
+version = _get_version('1.0.2', status=None)

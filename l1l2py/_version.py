39eb5ae9e285b080eb2a33aad88eb5fea01f137a
Salvatore Masecchia <salvatore.masecchia@disi.unige.it>
2011-01-25 13:33:49 +0100

    1.0.3 preparation

--- l1l2py/_version.py
+++ l1l2py/_version.py
@@ -1,13 +1,10 @@
 def _get_version(version, status=None):
     if status:
-        import datetime
-        now = datetime.datetime.now()
-        date = '.'.join('%02d'%x for x in (now.year, now.month, now.day))
-        return '%s%s-%s' % (version, status, date)
+       return '%s-%s' % (version, status)
     return version
 
 # major number for main changes
 # minor number for new features
 # release number for bug fixes and minor updates
 # status = {'alpha', 'beta', None}
-version = _get_version('1.1.0', status='alpha')
+version = _get_version('1.0.3', status=None)

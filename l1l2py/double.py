0482c08ba5ed86570f1d5731ac97751d3f1a2b4d
Salvatore Masecchia <salvatore.masecchia@gmail.com>
2011-08-12 10:01:47 +0200

    double step estimator intercept tests

--- l1l2py/double.py
+++ l1l2py/double.py
@@ -3,28 +3,36 @@ import numpy as np
 from .estimators import LinearModel
 
 class DoubleStepEstimator(LinearModel):
-    def __init__(self, selector, regressor, threshold=1e-10):
+    def __init__(self, selector, estimator, threshold=1e-10):
         self.selector = selector
-        self.regressor = regressor
+        self.estimator = estimator
         self.threshold = threshold
+        self.fit_intercept = estimator.fit_intercept
         self.coef_ = None
         self.intercept_ = None
         self.selected_ = None
+        # Intercept depends on 'estimator' params
     
     def fit(self, X, y):
+        X = np.asanyarray(X)
+        y = np.asanyarray(y)
+        
         # Selection        
         self.selector.fit(X, y)
         self.selected_ = (np.abs(self.selector.coef_) >= self.threshold)
         
         # Final Estimation
-        self.regressor.fit(X[:, self.selected_], y)
+        self.estimator.fit(X[:, self.selected_], y)
         
         # Coefficients
         self.coef_ = np.zeros_like(self.selector.coef_)
-        self.coef_[self.selected_] = self.regressor.coef_
-        self.intercept_ = self.regressor.intercept_
+        self.coef_[self.selected_] = self.estimator.coef_
+        self.intercept_ = self.estimator.intercept_
         
         return self
+    
+class DoubleStepEstimatorCV(LinearModel):
+    pass
 
         
         
--- l1l2py/tests/test_double.py
+++ l1l2py/tests/test_double.py
@@ -6,7 +6,7 @@ from nose.tools import *
 from ..double import DoubleStepEstimator
 from ..estimators import Ridge, Lasso
 
-def test_pipe():
+def test_double_optimization():
     """Test double optimization on a simple example."""
     # A simple sparse-sum function
     X = [[1, 2], [3, 4], [5, 6]]
@@ -23,7 +23,7 @@ def test_pipe():
     
     # Coefficients
     lasso = dstep.selector
-    ridge = dstep.regressor
+    ridge = dstep.estimator
     assert_array_almost_equal([0.90635646, 0.90635646], lasso.coef_[:2])
     assert_array_almost_equal([1.0, 1.0], ridge.coef_)
     assert_array_almost_equal([1.0, 1.0], dstep.coef_[:2])
@@ -31,5 +31,38 @@ def test_pipe():
     # Prediction
     y_ = dstep.predict(T)
     assert_array_almost_equal([15., 19., 3.], y_)
+    
+def test_double_optimization_intercept():
+    """Test double optimization soundness with different parameters."""
+    # A simple sum function with intercept
+    intercept = 2.0
+    X = [[1, 2], [3, 4], [5, 6]]
+    y = [sum(x)+intercept for x in X]
+    
+    # OLS cascade
+    dstep = DoubleStepEstimator(Ridge(mu=0.0, fit_intercept=True),
+                                Ridge(mu=0.0, fit_intercept=True)).fit(X, y)
+    assert_almost_equal(intercept, dstep.intercept_)
+    assert_array_almost_equal([1., 1.], dstep.coef_)
+    
+    # OLS cascade: first step without fit_intercept
+    dstep = DoubleStepEstimator(Ridge(mu=0.0, fit_intercept=False),
+                                Ridge(mu=0.0, fit_intercept=True)).fit(X, y)
+    assert_almost_equal(intercept, dstep.intercept_)
+    assert_array_almost_equal([1., 1.], dstep.coef_)
+    
+    # OLS cascade: second step without fit_intercept
+    dstep = DoubleStepEstimator(Ridge(mu=0.0, fit_intercept=True),
+                                Ridge(mu=0.0, fit_intercept=False)).fit(X, y)
+    assert_almost_equal(0.0, dstep.intercept_)
+    assert_array_almost_equal([-1., 3.], dstep.coef_)
+    assert_equal(False, dstep.fit_intercept)
+    
+    # OLS cascade: all steps without fit_intercept
+    dstep = DoubleStepEstimator(Ridge(mu=0.0, fit_intercept=False),
+                                Ridge(mu=0.0, fit_intercept=False)).fit(X, y)
+    assert_almost_equal(0.0, dstep.intercept_)
+    assert_array_almost_equal([-1., 3.], dstep.coef_)
+    
     
     
